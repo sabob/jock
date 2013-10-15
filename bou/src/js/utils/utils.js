698fabe4b61df9a9fee7c17cb4012424c9994408
sabob <sabob1@gmail.com>
2013-10-15 13:37:06 +0200

    init

---
+++ bou/src/js/utils/utils.js
@@ -0,0 +1,51 @@
+define(function(require) {
+    var $ = require("jquery");
+    require("jquery.deserialize");
+    require("domReady!");
+
+    function Utils() {
+        
+        //console.log("Utils:");
+        //$('body').alpha().beta();
+        
+        this.toJson = function(formId) {
+            var object = this.toObject(formId);
+            var json = JSON.stringify(object);
+            return json;
+        };
+        
+        this.fromJson = function(formId, json) {
+            var obj = JSON.parse(json);
+            this.fromObject(formId, obj);
+            return json;
+        };
+
+        this.toObject = function(formId) {
+            var result = {};
+            var array = $('#' + formId).serializeArray();
+            $.each(array, function() {
+                if (result[this.name]) {
+                    if (!result[this.name].push) {
+                        result[this.name] = [result[this.name]];
+                    }
+                    result[this.name].push(this.value || '');
+                } else {
+                    result[this.name] = this.value || '';
+                }
+            });
+            return result;
+        };
+        
+        this.fromObject = function(formId, obj) {
+             $('#' + formId).deserialize(obj);
+        };
+        
+        this.exist = function(val) {
+                if (typeof(val) === 'undefined' || val === null) {
+                    return false;
+                }
+                return true;
+            };
+    }
+    return new Utils();
+});
